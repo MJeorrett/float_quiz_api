cc2580beb8daad6e4ea57ded1ab09cf8ff67a5bc
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-02-03 18:04:13 +0000

    restricting access controll again

--- config/application.rb
+++ config/application.rb
@@ -14,11 +14,7 @@ module FloatQuizApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        # origins([
-        #   'http://localhost:3000',
-        #   'https://arcane-harbor-35765.herokuapp.com/'
-        # ])
-        origins '*'
+        origins 'http://localhost:3000', 'https://arcane-harbor-35765.herokuapp.com/'
         resource '*', :headers => :any, :methods => [:get]
       end
     end
