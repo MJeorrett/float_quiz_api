b4a6d593042061ffd36093b4da1192b2664b6006
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-02-07 11:38:01 +0000

    setting questions_controller test to check authentication required

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class QuestionsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get questions_index_url
-    assert_response :success
+    get 'http://localhost:5000/v1'
+    assert_response :unauthorized
   end
 
 end
